627e678442745e1333b3efcc2f381cb449739076
BrognietGeoffrey <58220770+BrognietGeoffrey@users.noreply.github.com>
2021-03-02 14:24:11 +0100

    Create .c for 18f458
    Juste la création du code pour l'allumage de leds quand le seuil est atteint

---
+++ C/program-18f458.c
@@ -0,0 +1,27 @@
+#include <program-18f458.h>
+#include <stdio.h>
+
+
+void main()
+{
+   setup_adc_ports(AN0);
+   setup_adc(ADC_CLOCK_INTERNAL);
+   setup_low_volt_detect(FALSE);
+
+   while(TRUE)
+   {
+   
+   
+   
+   
+      // Gestion des led selon le nombre de personne
+      if (valeur > seuil){
+         output_low(); //Dans la paranthèse faut indiquer la pin de la led verte car on fait diminuer l'intensité
+         output_toggle(); //Ici on doit indiquer la pin de la led rouge car on allume cette led car la valeur dépasse le seuil
+      }
+      else {
+         output_high(); //Vu qu'on doit tout le temps laisser la led verte allumé si la valeur ne dépasse pas le seuil, on la met en "haut"
+         output_low(); //Et vu qu'on doit laisser la led verte allumé en permanence, on éteins "down" la led rouge
+      }
+
+}
