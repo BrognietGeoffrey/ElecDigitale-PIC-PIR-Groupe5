61b1f7b39b039fcd55d4ae38b1d644ab241da58c
severinRobert <64019051+severinRobert@users.noreply.github.com>
2021-03-08 17:32:32 +0100

    Update program-18f458.c

--- C/program-18f458.c
+++ C/program-18f458.c
@@ -1,6 +1,15 @@
 #include <program-18f458.h>
 #include <stdio.h>
 
+int buffer;
+boolean flag = 0;
+
+#int_RDA
+void RDA_isr(void)
+{
+   buffer = int(getc());
+   flag = 1;
+}
 
 void main()
 {
@@ -10,8 +19,8 @@ void main()
    int PIR_IN= 15;// entree de la zone
    int PIR_OUT= 16;// sortie de la zone
 
-   setup_adc_ports(AN0); //pas besoin on utilise pas la pin AN0
-   setup_adc(ADC_CLOCK_INTERNAL);
+   setup_adc_ports(NO_ANALOGS); //pas besoin on utilise pas la pin AN0
+   setup_adc(ADC_OFF);
    setup_low_volt_detect(FALSE);
    setup_psp(PSP_DISABLED);
    setup_spi(FALSE);
@@ -28,19 +37,22 @@ void main()
       delay_ms(250);
       set_adc_channel(0);// pin non utiliser pas neccessaire 
       output_e(read_adc());
+
       //verifier si il y a un passage entree ou sortie
       if(input(PIR_IN)) {
-
          nb_personne += 1;
+         printf("Une personne en plus dans la zone : %d personnes dans la zone\n",nb_personne);
       }
       if(input(PIR_OUT) && nb_personne != 0) {
          nb_personne -= 1;
+         printf("Une personne en moins dans la zone : %d personnes dans la zone\n",nb_personne);
       }
    
       // Gestion des led selon le nombre de personne
       if (nb_personne > nb_max_personne){
          output_low(10); //Dans la paranthèse faut indiquer la pin de la led verte car on l'éteind
          output_toggle(9); //Ici on doit indiquer la pin de la led rouge car on allume cette led car la valeur dépasse le seuil
+         printf("Trop de personnes sont dans la zone : %d personnes dans la zone\n",nb_personne);
       }
       else {
          output_e(0b100);// met E0 à 0, E1 à 0 et E2 à 1
@@ -51,5 +63,11 @@ void main()
       int dizaine = nb_personne/10;
       output_b(dizaine);
       output_b((unite<<4));
+
+      // Gestion du port com
+      if(flag) {
+         nb_max_personnes = buffer;
+      }
+
    }
 }
