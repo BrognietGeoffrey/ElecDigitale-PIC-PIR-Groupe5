162189d282807782c3553752d2bf4b9386c1d253
Brogniet Geoffrey <58220770+BrognietGeoffrey@users.noreply.github.com>
2021-05-17 07:28:51 +0200

    Update gra.py

--- Python/gra.py
+++ Python/gra.py
@@ -2,10 +2,6 @@ from tkinter import *
 from tkinter import ttk
 import webbrowser
 import serial
-import serial.tools.list_ports as port_list
-
-
-
 import time
 
 # -------------- Partie programmation ------------------- #
@@ -15,6 +11,9 @@ url1 = 1
 url1name = "https://github.com/BrognietGeoffrey/ElecDigitale-PIC-PIR-Groupe5/tree/main/Python"
 url2 = 1
 url2name = "https://github.com/BrognietGeoffrey/ElecDigitale-PIC-PIR-Groupe5/tree/main/C"
+
+#Connexion pour le port COM
+ser = serial.Serial('COM4', baudrate=9600, timeout=1)
 
 
 # Boutons des codes et de fermeture
@@ -29,71 +28,43 @@ def openButtonC():
 def closeWindow():
     window.destroy()
 
-"""
-picData = serial.Serial('COM3', baudrate=9600, timeout=1)
-if not picData.isOpen():
-    picData.open()
-print('com4 is open', picData.isOpen())
-"""
-limit = 3
 
-"""
 # Fonction qui délimite le nombre de personne
-def limit():
+def maxPeople():
+
     read = spinbox.get()
-    picData.write(read.encode())
-    time.sleep(2)
+    ser.write(read.encode())
     return read
-"""
-
-def updatePeople():
-    serialPort = serial.Serial(port="COM4", baudrate=9600,
-                               bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-    com = serialPort.write(serialPort)
-    print(com)
-
-    if com != "":
-        print("ici")
-        com = com
-        if com < limit :
-            info = "Il y a {}".format(com)
-            labelText.config(text=info)  # On indique où placer le texte pour Tkinter
-        elif com >= limit :
-            info = "Attention, {} = trop !".format(com)
-            labelText.config(text=info)  # On indique où placer le texte pour Tkinter
-
-    window.after(100, updatePeople)
 
 
-"""
 # Fonction qui lit les données du PIC
 def readDataPic():
+
     # On reçoit du code Ascii donc on le décode
-    data = picData.readline().decode("ascii")
+    people = ser.readline().decode('ascii')
 
     # Si les données sont différentes de rien donc on imprime les valeurs
-    if data != "":
-        print("toto")
-        value = int(data)
+    if people != "":
+        value = int(people)
         print(value)
 
         # On instancie la variable limit avec notre fonction qui délimite le nombre de personnes
+        maximum = maxPeople()
 
         # Si la valeur est en dessous de la limite fixée dans la fonction limit(), alors on indique le message positif
-        if value < limit():
-            info = "Il y a {}".format(value)
-            labelText.config(text=info)  # On indique où placer le texte pour Tkinter
-
+        if value < int(maximum):
+            number = "Voici le nombre de personnes : {}".format(value)
+            info = "Le nombre de personne ne dépasse pas la limite"
+            labelNumber.config(text=number)
+            labelInfo.config(text=info)  # On indique où placer le texte pour Tkinter
         # Si la valeur dépasse la limite fixée dans la fonction limit(), alors on indique le message négatif
-        elif value >= limit():
-            info = "Attention, {} = trop !".format(value)
-            labelText.config(text=info)  # On indique où placer le texte pour Tkinter
+        elif value >= int(maximum):
+            number = "Voici le nombre de personnes : {}".format(value)
+            info = "Le nombre de personne dépasse la limite"
+            labelNumber.config(text=number)
+            labelInfo.config(text=info)  # On indique où placer le texte pour Tkinter
+    window.after(1, readDataPic)
 
-    else :
-        print("je suis là")
-
-    window.after(10, readDataPic)
-"""
 
 # --------------------- Partie graphique ---------------- #
 window = Tk()
@@ -112,8 +83,11 @@ buttonP.grid(column=0, row=1)
 buttonExit = Button(window, text='Fermer', command=closeWindow)
 buttonExit.grid(column=3, row=0)
 
-labelText = Label(window, text="")
-labelText.grid(column=1, row=1)
+labelNumber = Label(window, text="")
+labelNumber.grid(column=1, row=1)
+
+labelInfo = Label(window, text=" ")
+labelInfo.grid(column=1, row=2)
 
 labelSpinbox = Label(window, text="Selectionnez une limite : ")
 labelSpinbox.grid(column=0, row=2, padx=0, pady=10)
@@ -121,10 +95,11 @@ labelSpinbox.grid(column=0, row=2, padx=0, pady=10)
 spinbox = ttk.Spinbox(window, from_=0, to=10, width=10, state='readonly')
 spinbox.grid(column=0, row=3)
 
-buttonConfirm = Button(window, text="Confirmer", command=limit)
+buttonConfirm = Button(window, text="Confirmer", command=maxPeople)
 buttonConfirm.grid(column=0, row=4)
 
+window.update()
+
 if __name__ == '__main__':
-    print("tata")
-    window.after(10, updatePeople)
+    window.after(10, readDataPic)
     window.mainloop()
